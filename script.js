b5bbc860701c06b687839b43a75ca8b419c49bdb
alanbartleysgs <126794375+alanbartleysgs@users.noreply.github.com>
2024-08-09 18:03:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {  
+    const visitSgsButton = document.getElementById("visit-sgs-button");  
+    visitSgsButton.addEventListener("click", function() {  
+     window.location.href = "https://www.saintgeorgeschool.es/";  
+    });  
+  });
+document.addEventListener("DOMContentLoaded"),function(){
+    const visitSgsButton =
+}
